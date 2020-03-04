3146e99b6c824c9ce506c800aef11faac52acdaa
zzyyzzzzy <40347232+zzyyzzzzy@users.noreply.github.com>
2020-03-03 22:06:38 -0800

    first view text

--- App.js
+++ App.js
@@ -1,1 +1,11 @@
 import React from 'react';
+import {View, Text} from 'react-native';
+const App = () => {
+  return (
+    <View>
+      <Text>Hello world!</Text>
+    </View>
+  );
+};
+
+export default App;
