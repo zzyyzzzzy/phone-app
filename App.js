8229c1ebea4f9ca78fc5e2d0166d2093509e1704
zzyyzzzzy <40347232+zzyyzzzzy@users.noreply.github.com>
2020-03-04 20:12:38 -0800

    change style

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {View, Text} from 'react-native';
 const App = () => {
   return (
-    <View>
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
       <Text>Hello world!</Text>
     </View>
   );
