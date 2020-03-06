6dcc42b111790bccd47ed26b815bae094739381c
zzyyzzzzy <40347232+zzyyzzzzy@users.noreply.github.com>
2020-03-05 20:33:37 -0800

    add comp

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
+import Header from './component/Header';
 const App = () => {
   return (
     <View style={styles.container}>
-      <Text>Hello world!</Text>
+      <Header />
     </View>
   );
 };
