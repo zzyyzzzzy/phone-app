4f3cf90a284712960cd182387c556a50970dc9b5
zzyyzzzzy <40347232+zzyyzzzzy@users.noreply.github.com>
2020-03-05 19:26:19 -0800

    add more

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
 const App = () => {
   return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+    <View style={styles.container}>
       <Text>Hello world!</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
 
 export default App;
