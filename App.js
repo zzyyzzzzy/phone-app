cec56053506eef35a6d6d1e502646580e4daf060
zzyyzzzzy <40347232+zzyyzzzzy@users.noreply.github.com>
2020-03-16 18:13:25 -0700

    add default prop

--- App.js
+++ App.js
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 import Header from './component/Header';
+import {uuid} from 'uuidv4';
+
 const App = () => {
+  const [item, setItem] = useState([
+    {id: 1, text: 'Milk'},
+    {id: 2, text: 'Eggs'},
+    {id: 3, text: 'Coke'},
+    {id: 4, text: 'Juice'},
+  ]);
   return (
     <View style={styles.container}>
       <Header />
@@ -12,6 +20,7 @@ const App = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    paddingTop: 60,
   },
 });
 
