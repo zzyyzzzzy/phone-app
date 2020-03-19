5f4344abefd2bb6ee89e47898ad42e260a46a0ed
zzyyzzzzy <40347232+zzyyzzzzy@users.noreply.github.com>
2020-03-19 15:09:07 -0700

    add  style and icons

--- components/ListItem.js
+++ components/ListItem.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
+import Icon from 'react-native-vector-icons/dist/FontAwesome';
 const ListItem = ({item}) => {
   return (
     <TouchableOpacity style={styles.listItem}>
       <View style={styles.listItemView}>
         <Text style={styles.listItemText}>{item.text}</Text>
+        <Icon name="remove" size={20} color="firebrick" />
       </View>
     </TouchableOpacity>
   );
@@ -16,6 +18,14 @@ const styles = StyleSheet.create({
     backgroundColor: '#f8f8f8',
     borderBottomWidth: 1,
     borderColor: '#eee',
+  },
+  listItemView: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  listItemText: {
+    fontSize: 18,
   },
 });
 
